ff96058ac84ca2e4ccc0bf3b4108aaf75b69aae1
Abdul Muthallib <sm2769@srmist.edu.in>
2023-08-22 20:54:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TrainListPage from './TrainListPage';
+import SingleTrainPage from './SingleTrainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={TrainListPage} />
+        <Route path="/train/:trainId" component={SingleTrainPage} />
+      </Routes>
+    </Router>
   );
 }
 
